3a5ac94d85609bb4b93bd58754afadcf94163f1a
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-01-05 15:02:15 +0300

    Deleting Unnecessary Functions and Namespaces in MainMenu Class

--- Assets/Scripts/Scenes/MainMenu.cs
+++ Assets/Scripts/Scenes/MainMenu.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,11 +8,6 @@ namespace FTRGames.HugoLuLuLu
         public void PlayBtnClick()
         {
             SceneManager.LoadScene("1-Intro");
-        }
-
-        public void SettingsBtnClick()
-        {
-
         }
 
         public void QuitBtnClick()
